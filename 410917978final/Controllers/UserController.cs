d5c07cd7833dcede3f731e6f422997a9143db3c0
陳宛莛 <wanda5982266@gmail.com>
2023-06-07 19:23:06 +0800

    123

---
+++ 410917978final/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _410917978final.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index(string name,float music)
+        {
+            return View();
+        }
+    }
+}
